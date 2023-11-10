fd7fab41d6ae70dd11b5c84ed278181d7c374538
Test <Hammertyme101@gmail.com>
2023-11-10 00:00:03 -0500

    check2

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://root:root@cluster0.h2uoso0.mongodb.net/books-collection?retryWrites=true&w=majority'
--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 import express from "express";
-import { PORT } from "./config.js"
+import { PORT, mongoDBURL } from "./config.js"
+import mongoose from "mongoose"
+import { book } from './models/bookmodel.js'
 const app = express();
-
-app.listen(PORT, () =>{
-    console.log(`App is listening to port: ${PORT}`);
-
+ç
+app.get('/', (request, response)=>{
+    console.log(request);
+    return response.status(234).send('Welcome to the tutorial')
 });
+
+app.post('/books', async (request, response)=>{
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+    }
+    catch (error){
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log("app connected to database");
+    app.listen(PORT, () =>{
+        console.log(`App is listening to port: ${PORT}`);
+    
+    });
+})
+.catch((error)=>{
+    console.log(error);
+})
